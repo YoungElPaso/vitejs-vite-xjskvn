5d1481ce235d811a16a2dd2157bd86330c1ced2f
YoungElPaso <jesse.a.sutherland@gmail.com>
2022-09-06 15:19:52 -0400

    2 files modified, 2 added, 2 deleted

--- src/css-importer.ts
+++ src/css-importer.ts
@@ -1,8 +1,8 @@
 // Import Lit CSS utilities.
 import { CSSResult, unsafeCSS } from 'lit';
 
-// Import the CSS common to all web components for re-use.
-import styles from './index.css?inline';
+// Import the CSS common to all web components for possible re-use *inside* the ShadowDOM.
+import styles from './wc-common.css?inline';
 
 /**
  * Exports a CSSResult from imported CSS file. Useful for sharing styles across components.
