035e1fe4c5abce3ecaefa743f645f44d5deee44c
YoungElPaso <jesse.a.sutherland@gmail.com>
2022-09-08 16:24:55 -0400

    2 files modified

--- src/extra-details.ts
+++ src/extra-details.ts
@@ -4,9 +4,9 @@ import {
   html,
   css,
   property,
+  query,
   queryAssignedElements,
 } from 'lit-element';
-
 import { sharedWCStyles } from './css-importer';
 
 @customElement('mds-extra-details')
@@ -17,15 +17,15 @@ export class ExtraDetails extends LitElement {
     'inactive' || 'active';
   // TODO: tie this to details 'open' attr by passing down?
 
+  // Get the child details element.
   @queryAssignedElements({ selector: 'details' })
-  // Have to losely type a bit otherwise type errors on some attributes for returned HTMLElement.
-  _summary!: Array<HTMLElement | any>;
+  _details!: Array<HTMLDetailsElement>;
 
   // TODO: this handler needs to be added to the <summary> tag which is a part of the child markup slotted.
 
   // TODO: need to grab refs to some of the child elements to attach handlers, update attributes etc. Read how to do that!
   handleClick() {
-    console.log(this._summary);
+    console.log(this._details);
   }
 
   // TODO: also need to add animation to open/close. For that need to compute heights and then transition between them - so dynamically set style based on children? Could calculate on firstUpdate maybe?
@@ -35,18 +35,53 @@ export class ExtraDetails extends LitElement {
   // TODO: also need to check if children of details are 'active' if so bubble up an event to set the property? Sure. sounds good.
 
   firstUpdated() {
+    // Add an event listener to handle clicks on the details element and update properties.
+    let t = this;
+    this.addEventListener('click', function (e) {
+      console.log('event', e, t.isActive);
+      t.isActive = t._details[0].open ? 'active' : 'inactive';
+    });
+
     // super.firstUpdated();
-    // console.log(this._summary[0]);
-    // this._summary[0].onclick = function () {
+    // console.log(this._details[0]);
+    // this._details[0].onclick = function () {
     //   console.log('summary!');
     // };
-    // OK, cool, so can pass prop down via attribute like this!?
-    this._summary[0].open = this.isActive == 'active' ? true : false;
+
+    // console.log('summary', this._summary);
+
+    // this._details[0].open = this.isActive == 'active' ? true : false;
+
+    // Evaluate some stuff about the children and set some props. 1) Set isActive=active if childNodes contain a 'selected' or 'active' class only if isActive is 'inactive' already and the initial value. 2) Get the summary element and get it's height - can use that as initialHeight for animation.
+
+    console.log('details', this._details);
+
+    // See if there are any active children.
+    let activeChild: NodeList | null =
+      this._details[0].querySelectorAll('.selected, .active');
+
+    // TODO: this should probably be replaced by an event listener and instead I should simulate a click on the summary - that will keep the details open attr and isActive in sync I think and take advantage of event bubbling from child on up, while we set intial state from parent down!
+    // this.isActive = activeChild ? 'active' : 'inactive';
+    // console.log(activeChild, this.isActive);
+    let summaryElement: HTMLElement | null =
+      this._details[0].querySelector('summary');
+
+    // If initial state is inactive and there's a summary and there's also an 'active' child element, then open the details by doing a click event.
+    if (
+      this.isActive == 'inactive' &&
+      activeChild.length > 0 &&
+      summaryElement
+    ) {
+      console.log('sumEl', summaryElement);
+      summaryElement.click();
+    }
   }
 
   // TODO: scrap the onclick stuff? Details already handles that? Can just hook into attribute changing there and bubble up event? Or just listen to that event at a higher level? Probably the better way to go! Then just need a way to pass down prop...probably can do w/ firstUpdated?
 
   render() {
+    // OK, cool, so can pass prop down via attribute like this!? TODO: this has to be in another lifecycle hook cause _details is undefined here.
+    // this._details[0].open = this.isActive == 'active' ? true : false;
     return html`
     <slot>
     </slot>
