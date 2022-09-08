131b4b0c9e9878317b001dc35dad7fc52abc1a97
YoungElPaso <jesse.a.sutherland@gmail.com>
2022-09-07 20:34:15 -0400

    1 file modified

--- src/css-importer.ts
+++ src/css-importer.ts
@@ -1,10 +1,10 @@
-// Import Lit CSS utilities.
+// Import Lit CSS utility and intialization styles.
 import { CSSResult, unsafeCSS } from 'lit';
 
-// Import the CSS common to all web components for possible re-use *inside* the ShadowDOM. Uses ?inline so the CSS is added as a string.
+// Import the CSS common to all web components for possible re-use *inside* the ShadowDOM. Uses ?inline so the CSS is bundled.
 import styles from './wc-common.css?inline';
 
 /**
- * Exports a CSSResult from imported CSS file. Useful for sharing styles across components.
+ * Exports a Lit CSSResult from imported CSS file. Useful for sharing styles across components.
  */
 export default unsafeCSS(styles) as CSSResult;
