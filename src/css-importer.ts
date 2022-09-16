c14bd8891c77236da4e5ca0cf101d00757c24535
YoungElPaso <jesse.a.sutherland@gmail.com>
2022-09-16 16:25:15 -0400

    1 file modified

--- src/css-importer.ts
+++ src/css-importer.ts
@@ -1,7 +1,7 @@
 // Import Lit unsafeCSS method.
 import { unsafeCSS } from 'lit';
 
-// Import the CSS common to all web components for possible re-use *inside* the ShadowDOM. Uses ?inline so the CSS is bundled. TODO: revisit this comment...?
+// Import the CSS common to all web components for possible re-use *inside* the ShadowDOM. Uses ?inline param so the CSS is bundled by Vite.
 import styles from './wc-common.css?inline';
 
 /**
